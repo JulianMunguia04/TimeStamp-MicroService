439a88ca81f87d117602160fcf7edc94e8ed29f6
JulianMunguia04 <julianmunguia04@gmail.com>
2025-04-17 13:30:58 -0600

    Final

--- index.js
+++ index.js
@@ -20,8 +20,20 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const date = new Date(req.params.date)
+  let result;
+  if (isNaN(date.getTime())){
+    result = {error: "Invalid Date" }
+  }
+  else{
+    result = {
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    } 
+  }
+  
+  res.json(result);
 });
 
 
